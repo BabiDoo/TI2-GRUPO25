15176cfc2a256e2ed9165e3b3b07778b17cb7d0f
Barbara <barbara.laisdorneles@gmail.com>
2025-05-09 06:07:56 -0300

    ajusta conexao com postgresql

--- Sprint2/Backend/db.js
+++ Sprint2/Backend/db.js
@@ -1,4 +1,6 @@
+const { Pool } = require('pg');
 require('dotenv').config();
+
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -6,3 +8,5 @@ const pool = new Pool({
   password: process.env.DB_PASS,
   port: process.env.DB_PORT
 });
+
+module.exports = pool;
