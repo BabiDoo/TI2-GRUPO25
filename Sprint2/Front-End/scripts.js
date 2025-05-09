a614b81ec9d8d380ffba61e6411d5d85d0ca14e7
Barbara <barbara.laisdorneles@gmail.com>
2025-05-09 02:09:09 -0300

    add botao de login

--- Sprint2/Front-End/scripts.js
+++ Sprint2/Front-End/scripts.js
@@ -1,5 +1,21 @@
 const cadastroButton = document.querySelector('#cadastrarBotao');
+if (cadastroButton) {
+    cadastroButton.addEventListener('click', () => {
+        window.location.href = 'cadastro.html';
+    });
+}
 
-cadastroButton.addEventListener('click', () => {
-    window.location.href = 'cadastro.html';
-});
+
+const entrarButton = document.querySelector('.entrarBotao');
+if (entrarButton) {
+    entrarButton.addEventListener('click', () => {
+        const matriculaInput = document.querySelector('input[type="text"]');
+        const matricula = matriculaInput ? matriculaInput.value.trim() : "";
+
+        if (matricula.toLowerCase() === "admin") {
+            window.location.href = 'homeAdm.html';
+        } else {
+            window.location.href = 'home.html';
+        }
+    });
+}
